bd7e99706484323d0655def0f0e51b942c3ee1cf
Elias Gander <gander.elias@yahoo.de>
2025-02-18 14:55:38 +0100

    wiener parkflaechen aktualisieren

--- data-visualization/wiener-parkplaetze/script.js
+++ data-visualization/wiener-parkplaetze/script.js
@@ -1,33 +1,74 @@
-var map = L.map("map").setView([48.2082, 16.3738], 13); // Vienna center
-var minZoom = 17; // Show parking spots only at zoom 15+
-var tileSize = 0.005; // 500m in degrees (~WGS84 near Vienna)
-var loadedTiles = new Map(); // Store loaded tiles
-var displayedTiles = new Map();
-var gridBounds = {
+class StreetObject {
+  constructor(name, id, assetName, takesUpFullSpot = false) {
+    this.name = name;
+    this.id = id;
+    this.assetName = assetName;
+    this.takesUpFullSpot = takesUpFullSpot;
+  }
+}
+
+const parkingSpot = new StreetObject("Parkplatz", 1, "parkplatz", true);
+const tree = new StreetObject("Baum", 2, "baum");
+const flowerBed = new StreetObject("Blumenbeet", 3, "blumenbeet");
+const raisedBed = new StreetObject("Hochbeet", 4, "hochbeet");
+const seating = new StreetObject("Sitzgelegenheit", 5, "sitzgelegenheit");
+const sidewalk = new StreetObject("Gehsteig", 6, "gehsteig");
+const schanigarten = new StreetObject("Schanigarten", 7, "schanigarten", true);
+const bikeLane = new StreetObject("Radweg", 8, "radweg");
+const bikeStand = new StreetObject("Radabstellplatz", 9, "radabstellplatz");
+const nothing = new StreetObject("Freifläche", 10, "freiflaeche");
+const streetObjects = [
+  parkingSpot,
+  tree,
+  flowerBed,
+  raisedBed,
+  seating,
+  sidewalk,
+  schanigarten,
+  bikeLane,
+  bikeStand,
+  nothing,
+];
+const minZoom = 17;
+const maxZoom = 21;
+const tileSize = 0.005;
+const gridBounds = {
   minx: 16.20183574,
   miny: 48.12302008,
   maxx: 16.5464744,
   maxy: 48.31235361,
 };
+const assetPixels = 159;
+const zoomHintElement = document.getElementById("zoom-hint");
+const loadingIndicatorElement = document.getElementById("loading-indicator");
+const infosLinkElement = document.getElementById("infos-link");
+const infosElement = document.getElementById("infos");
+const remainingParkingSpotsElement = document.getElementById("stats-remaining");
+const usedParkingSpotsElement = document.getElementById("stats-used");
+const actionsElement = document.getElementById("actions");
+const map = new maplibregl.Map({
+  container: "map",
+  style: "https://tiles.stadiamaps.com/styles/stamen_toner_lite.json",
+  center: [16.3738, 48.2082],
+  zoom: 12,
+  maxZoom: maxZoom,
+  minZoom: 10,
+  maxBounds: [
+    [gridBounds.minx, gridBounds.miny],
+    [gridBounds.maxx, gridBounds.maxy],
+  ],
+});
+var remainingParkingSpots = 258205;
+var usedParkingSpots = 0;
+var selectedFeature;
 
-maplibregl.setRTLTextPlugin(
-  "https://unpkg.com/@mapbox/mapbox-gl-rtl-text@0.2.3/mapbox-gl-rtl-text.min.js"
-);
-L.maplibreGL({
-  style:
-    "https://tiles-eu.stadiamaps.com/styles/stamen_toner_lite.json?api_key=9cb5a2cf-4b74-4e87-ade1-320768b874d4",
-  attribution:
-    '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-}).addTo(map);
-
-function getVisibleTiles() {
+function getVisibleTileIds() {
   var bounds = map.getBounds();
   var minx = bounds.getWest();
   var miny = bounds.getSouth();
   var maxx = bounds.getEast();
   var maxy = bounds.getNorth();
-
-  var tiles = [];
+  var tileIds = [];
   for (
     var x = Math.floor((minx - gridBounds.minx) / tileSize);
     x <= Math.floor((maxx - gridBounds.minx) / tileSize);
@@ -38,55 +79,208 @@ function getVisibleTiles() {
       y <= Math.floor((maxy - gridBounds.miny) / tileSize);
       y++
     ) {
-      tiles.push(`x${x}_y${y}.geojson`);
+      tileIds.push(`x${x}_y${y}`);
     }
   }
-  return tiles;
+  return tileIds;
 }
 
-function onEachFeature(feature, layer) {
-  layer.on("click", function (e) {
-    console.log(e);
+function getPixelCount(metres, zoom) {
+  const lat = map.getCenter().lat;
+  const earthCircumference = 40075016.686;
+  const metersPerPixel =
+    (earthCircumference * Math.cos((lat * Math.PI) / 180)) /
+    Math.pow(2, zoom + 8);
+  return metres / metersPerPixel;
+}
+
+function format(number) {
+  return new Intl.NumberFormat("de-DE", { useGrouping: "always" }).format(
+    number
+  );
+}
+
+function placeStreetObject(newStreetObjectId, newTakesUpFullSpot) {
+  if (!selectedFeature) {
+    return;
+  }
+  const source = map.getSource(selectedFeature.source);
+  const data = JSON.parse(JSON.stringify(source._data));
+  data.features = data.features.map((feature) => {
+    if (feature.id === selectedFeature.id) {
+      const currentStreetObjectId =
+        feature.properties.streetObjectId ?? parkingSpot.id;
+      if (
+        currentStreetObjectId === parkingSpot.id &&
+        newStreetObjectId !== parkingSpot.id
+      ) {
+        usedParkingSpotsElement.textContent = format(++usedParkingSpots);
+        remainingParkingSpotsElement.textContent = format(
+          --remainingParkingSpots
+        );
+      }
+      if (
+        currentStreetObjectId !== parkingSpot.id &&
+        newStreetObjectId === parkingSpot.id
+      ) {
+        usedParkingSpotsElement.textContent = format(--usedParkingSpots);
+        remainingParkingSpotsElement.textContent = format(
+          ++remainingParkingSpots
+        );
+      }
+      const currentTakesUpFullSpot = streetObjects.find(
+        (obj) => obj.id === currentStreetObjectId
+      ).takesUpFullSpot;
+      if (
+        newTakesUpFullSpot ||
+        currentTakesUpFullSpot ||
+        feature.properties.streetObjectId2
+      ) {
+        feature.properties.streetObjectId = newStreetObjectId;
+        feature.properties.streetObjectId2 = undefined;
+      } else {
+        feature.properties.streetObjectId2 = newStreetObjectId;
+      }
+    }
+    return feature;
   });
+  source.setData(data);
 }
 
 async function loadTiles() {
-  if (map.getZoom() >= minZoom) {
-    var neededTileIds = getVisibleTiles();
+  if (map.getZoom() < minZoom) {
+    return;
+  }
 
-    for (const tileId of neededTileIds) {
-      if (!loadedTiles.get(tileId)) {
-        response = await fetch(`parkplaetze/${tileId}`);
-        if (!response.ok) throw new Error("Tile not found");
-        data = await response.json();
-        loadedTiles.set(
-          tileId,
-          await L.geoJSON(data, {
-            style: { color: "blue", weight: 1, opacity: 0.7 },
-            onEachFeature: onEachFeature,
-          })
-        );
-      }
-    }
-
-    for (let [tileId, tile] of displayedTiles) {
-      if (!neededTileIds.includes(tileId)) {
-        map.removeLayer(tile);
-        displayedTiles.delete(tileId);
-      }
-    }
-    neededTileIds
-      .filter((tileId) => !displayedTiles.get(tileId))
-      .forEach((tileId) => {
-        tile = loadedTiles.get(tileId);
-        map.addLayer(tile);
-        displayedTiles.set(tileId, tile);
+  var neededTileIds = getVisibleTileIds();
+  for (const tileId of neededTileIds) {
+    if (!map.getSource(tileId)) {
+      response = await fetch(`parkplaetze/${tileId}.geojson`);
+      data = await response.json();
+      map.addSource(tileId, { type: "geojson", data: data });
+      map.addLayer({
+        id: tileId,
+        type: "fill",
+        source: tileId,
+        minzoom: minZoom,
+        paint: {
+          "fill-color": "blue",
+          "fill-opacity": [
+            "case",
+            ["boolean", ["feature-state", "isSelected"], false],
+            0.2,
+            0,
+          ],
+        },
       });
-  } else {
-    displayedTiles.forEach((value, _) => map.removeLayer(value));
-    displayedTiles = new Map();
+      streetObjects.forEach((obj) => {
+        const layer = {
+          id: `${tileId}_${obj.assetName}`,
+          type: "symbol",
+          source: tileId,
+          minzoom: minZoom,
+          filter: ["==", ["coalesce", ["get", "streetObjectId"], 1], obj.id],
+          layout: {
+            "icon-image": ["literal", obj.assetName],
+            "icon-size": [
+              "interpolate",
+              ["exponential", 2],
+              ["zoom"],
+              minZoom,
+              getPixelCount(5, minZoom) / assetPixels,
+              maxZoom,
+              getPixelCount(5, maxZoom) / assetPixels,
+            ],
+            "icon-rotate": ["get", "rotation"],
+            "icon-allow-overlap": true,
+            "icon-padding": 0,
+            "icon-anchor": obj.takesUpFullSpot ? "center" : "left",
+          },
+        };
+        map.addLayer(layer);
+        if (!obj.takesUpFullSpot) {
+          var additionalLayer = structuredClone(layer);
+          additionalLayer.id += "_2";
+          (additionalLayer.filter = [
+            "==",
+            ["coalesce", ["get", "streetObjectId2"], 1],
+            obj.id,
+          ]),
+            (additionalLayer.layout["icon-anchor"] = "right");
+          map.addLayer(additionalLayer);
+        }
+      });
+
+      map.on("mouseenter", tileId, function () {
+        map.getCanvas().style.cursor = "pointer";
+      });
+      map.on("mouseleave", tileId, function () {
+        map.getCanvas().style.cursor = "";
+      });
+    }
   }
 }
 
-map.on("zoomend moveend", loadTiles);
-loadTiles();
+usedParkingSpotsElement.textContent = format(usedParkingSpots);
+remainingParkingSpotsElement.textContent = format(remainingParkingSpots);
+actionsElement.innerHTML = streetObjects
+  .map(
+    (obj) =>
+      `<button onclick="placeStreetObject(${obj.id}, ${obj.takesUpFullSpot})"><img src="assets/${obj.assetName}.png"><span>${obj.name}</span></button>`
+  )
+  .join("");
+infosLinkElement.addEventListener("click", () => {
+  if (infosElement.classList.contains("visible")) {
+    infosElement.classList.remove("visible");
+    infosLinkElement.textContent = "?";
+  } else {
+    infosElement.classList.add("visible");
+    infosLinkElement.textContent = "X";
+  }
+});
+map.on("load", async () => {
+  map.on("sourcedata", (e) => {
+    loadingIndicatorElement.classList.remove("hidden");
+  });
+  map.on("idle", () => {
+    loadingIndicatorElement.classList.add("hidden");
+  });
+
+  for (let obj of streetObjects) {
+    const response = await map.loadImage(`assets/${obj.assetName}.png`);
+    map.addImage(obj.assetName, response.data);
+  }
+
+  map.on("moveend", loadTiles);
+  map.on("zoom", () => {
+    if (map.getZoom() >= minZoom) {
+      zoomHintElement.classList.add("hidden");
+    } else {
+      zoomHintElement.classList.remove("hidden");
+      if (selectedFeature) {
+        map.setFeatureState(selectedFeature, { isSelected: false });
+        selectedFeature = undefined;
+        actionsElement.classList.add("hidden");
+      }
+    }
+  });
+  map.on("click", (e) => {
+    if (selectedFeature) {
+      map.setFeatureState(selectedFeature, { isSelected: false });
+      selectedFeature = undefined;
+    }
+    const feature = map
+      .queryRenderedFeatures(e.point)
+      .find((f) => f.layer.type === "fill" && f.source.match(/^x\d+_y\d+$/));
+    if (!feature) {
+      actionsElement.classList.add("hidden");
+      return;
+    }
+    const newSelectedFeature = { source: feature.source, id: feature.id };
+    map.setFeatureState(newSelectedFeature, { isSelected: true });
+    selectedFeature = newSelectedFeature;
+    actionsElement.classList.remove("hidden");
+  });
+
+  await loadTiles();
+});
